fa5700c25020f731be3dd7577b1caadeaa8922a9
ArthurAre <arturito.ab3@gmail.com>
2024-05-20 15:16:47 -0600

    tarea 4

---
+++ Email/src/main/java/com/api/Email/EmailApplication.java
@@ -0,0 +1,24 @@
+package com.api.Email;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+@SpringBootApplication
+public class EmailApplication {
+	@Autowired
+	private EmailSenderService senderService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailApplication.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void sendMail(){
+		senderService.sendEmail("yoel.dyordan@gmail.com", 
+		"El subjeto","El cuerpo");
+	}
+
+}
